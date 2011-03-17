251fff914388e97a773b9ad6ade980b079e2daa6
joenmaes <joenmaes@gmail.com>
2011-03-17 16:41:56 +0100

    new BackendLinkCheckerHelper()

--- default_www/backend/modules/link_checker/cronjobs/check_links.php
+++ default_www/backend/modules/link_checker/cronjobs/check_links.php
@@ -16,7 +16,8 @@ class BackendLinkCheckerCronjobCheckLinks extends BackendBaseCronjob
 	 *
 	 * @var bool
 	 */
-	private $insertWorkingLinks = true;
+	private $insertWorkingLinks = false;
+
 
 	/**
 	 * Cleanup database
@@ -40,6 +41,9 @@ class BackendLinkCheckerCronjobCheckLinks extends BackendBaseCronjob
 		// cleanup database
 		$this->cleanupDatabase();
 
+		// require the helper class
+		require_once BACKEND_MODULES_PATH . '/link_checker/engine/helper.php';
+
 		// get data
 		$this->checkLinks();
 	}
@@ -59,20 +63,13 @@ class BackendLinkCheckerCronjobCheckLinks extends BackendBaseCronjob
 		$links = BackendModel::getDB(true)->getRecords($query);
 
 		// loop every link if there are any
-		if(isset($links)){
+		if(isset($links))
+		{
 
 			foreach ($links as $link)
 			{
-				// @todo	remember to remove debug code later on, cronjobs shouldn't generate output unless they're exceptions (those are auto-mailed).
-				echo '---' . "\r\n";
-				echo $link['module'] . "\r\n";
-				echo '---' . "\r\n";
-
-				// initialize
-				$ch = curl_init();
-
 				// built the array to insert
-				$values = array();
+				$values = array ();
 				$values['title'] = $link['title'];
 				$values['module'] = $link['module'];
 				$values['external'] = $link['external'];
@@ -81,48 +78,20 @@ class BackendLinkCheckerCronjobCheckLinks extends BackendBaseCronjob
 				$values['private_url'] = $link['private_url'];
 				$values['url'] = $link['url'];
 
-				// set the options, including the url
-				curl_setopt($ch, CURLOPT_URL, $values['url']);
-	            curl_setopt($ch, CURLOPT_HEADER, 1);
-	            curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-				// follow redirections
-				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-				// do not need the body. this saves bandwidth and time
-				curl_setopt($ch, CURLOPT_NOBODY, 1);
+				// check the link and retrieve the http error code
+				$values['code'] = BackendLinkCheckerHelper::checkLink($link['url']);
 
-				// execute and fetch the resulting HTML output
-				curl_exec($ch);
-
-				// get the info on the curl handle
-				$chinfo = curl_getinfo($ch);
-
-				// free up the curl handle
-				curl_close($ch);
-
-				$values['code'] = $chinfo['http_code'];
+				// remove the 'http://' before insert
 				$values['url'] = str_replace('http://', '', $values['url']);
 
 				// @todo	remember to remove debug code later on, cronjobs shouldn't generate output unless they're exceptions (those are auto-mailed).
 				echo $values['url'] .' => '. $values['code'] . "\r\n";
 
-				if (!$chinfo['http_code'])
+				// only insert dead or non working links
+				if(!$values['code'] || $values['code'] >= 400 && $values['code'] < 600 || $this->insertWorkingLinks)
 				{
-					// dead/faulty/non existing link?
+					// insert into database
 					BackendModel::getDB(true)->insert('crawler_results', $values);
-				}
-				else if ($chinfo['http_code'] >= 400 && $chinfo['http_code'] < 600)
-				{
-					// 4xx, 5xx error
-					BackendModel::getDB(true)->insert('crawler_results', $values);
-				}
-				if($this->insertWorkingLinks)
-				{
-					if ($chinfo['http_code'] >= 200 && $chinfo['http_code'] < 400)
-					{
-						// 2xx, 3xx working
-						BackendModel::getDB(true)->insert('crawler_results', $values);
-					}
 				}
 			}
 		}
